cb40bf1350ec548953672d358dc30bd0102b246b
Paul M Edwards <pauledwards@gmail.com>
2019-11-05 22:22:00 -0800

    Added Followers & Following sections. Added Emoji module to get rid of jsx-a11y/accessible-emoji warning.

--- github-user-card/src/App.js
+++ github-user-card/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import Axios from "axios";
+import Axios from 'axios';
+import Emoji from 'a11y-react-emoji';
 
 import './App.css';
 
@@ -7,53 +8,93 @@ class App extends React.Component {
   constructor() {
     super();
     this.state = {
-      github_data: []
+      myGitHubData: {},
+      myFollowers: [],
+      myFollowing: []
     };
   }
 
   componentDidMount() {
     let self = this;
-    async function fetchGitHubData() {
-      await Axios.get("https://api.github.com/users/PaulMEdwards")
+
+    async function fetchData(returnBucket, remoteDataSourceUri) {
+      await Axios.get(remoteDataSourceUri)
       .then(res => {
-        let r = res.data;
-        console.log('res: ', r);
-        self.setState({ github_data: r });
+        let d = res.data;
+        console.log(returnBucket+': ', d);
+        self.setState({ [returnBucket]: d });
       })
-      .catch(err => console.log("Error: ", err));
+      .catch(e => console.log("Error: ", e));
     }
-    fetchGitHubData();
+
+    fetchData('myGitHubData', "https://api.github.com/users/PaulMEdwards");
+    fetchData('myFollowers', "https://api.github.com/users/PaulMEdwards/followers");
+    fetchData('myFollowing', "https://api.github.com/users/PaulMEdwards/following");
   }
+
   render() {
+    async function returnCardData(remoteDataSourceUri) {
+      await Axios.get(remoteDataSourceUri)
+      .then(res => {
+        let d = res.data;
+        // return card(d);
+        return d;
+      })
+      .catch(e => console.log("Error: ", e));
+    }
+
+    function card(data) {
+      return (
+        <div className="card" key={data.id}>
+          <img src={data.avatar_url} alt="Avatar" />
+          <div>
+            <h3 className="name">{data.name != null ? data.name : data.login}</h3>
+            <p className="username">{data.login}</p>
+            <p>{data.location != null ? `Location: ${data.location}` : null}</p>
+            <p>Profile: <a href={data.html_url}>{data.html_url}</a></p>
+            <p>Followers: <a href={data.html_url+"?tab=followers"}>{data.followers}</a></p>
+            <p>Following: <a href={data.html_url+"?tab=following"}>{data.following}</a></p>
+            <p>{data.bio != null ? `Bio: ${data.bio}` : null}</p>
+          </div>
+        </div>
+      );
+    }
+
     console.log('this.state: ', this.state);
-    let data = this.state.github_data;
+    let data = this.state.myGitHubData;
     console.log('data: ', data);
 
     return (
       <div className="container">
         <div className="header">
           <img src="./assets/lambdalogo.png" alt="Lambda Logo"/>
-          <p><span role="img" area-label="loves">❤️'s</span></p>
+          <p><Emoji symbol="❤️" label="love" />'s</p>
           <img src="./assets/githublogo.png" alt="GitHub Logo" />
         </div>
         <div className="cards">
+          <h2>Me</h2>
           {
-            // this.state.github_data.map(d => <img src={d.avatar_url} key={d.node_id} alt="avatar" />)
+            card(data)
+          }
 
-            // this.state.github_data.map(data => {
-              <div className="card">
-                <img src={data.avatar_url} key={data.node_id} />
-                <div>
-                  <h3 className="name">{data.name != null ? data.name : data.login}</h3>
-                  <p className="username">{data.login}</p>
-                  <p>{data.location != null ? `Location: ${data.location}` : null}</p>
-                  <p>Profile: <a href="">{data.html_url}</a></p>
-                  <p>Followers: <a href={data.html_url+"?tab=followers"}>{data.followers}</a></p>
-                  <p>Following: <a href={data.html_url+"?tab=following"}>{data.following}</a></p>
-                  <p>{data.bio != null ? `Bio: ${data.bio}` : null}</p>
-                </div>
-              </div>
-            // })
+          <h2>Followers</h2>
+          {
+            this.state.myFollowers.map(data => {
+              // let d = returnCardData("https://api.github.com/users/"+data.login);
+              // console.log('d: ', d);
+              return (
+                card(data)
+              );
+            })
+          }
+
+          <h2>Following</h2>
+          {
+            this.state.myFollowing.map(data => {
+              return (
+                card(data)
+              );
+            })
           }
         </div>
       </div>
